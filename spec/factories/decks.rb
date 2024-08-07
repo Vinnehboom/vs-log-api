069feb4c1e9bdeb4c22680ed8b4e4fe2ca6bc316
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-08-07 16:33:51 +0100

    Add Importer setup with Deck Dto

---
+++ spec/factories/decks.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :deck do
+    id { Faker::Internet.uuid }
+    name { Faker::Game.title }
+    user_id { Faker::Internet.uuid }
+    archetype { Faker::Types.rb_hash }
+  end
+end
---
+++ spec/services/data_import/importer_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+module DataImport
+
+  RSpec.describe Importer do
+    let(:parser_double) { instance_double(Parser) }
+    let(:dto_class_double) { class_double(DeckDto) }
+    let(:dto_double) { instance_double(DeckDto) }
+    let(:importer) { described_class.new(parser: parser_double, dto_class: dto_class_double) }
+
+    before do
+      allow(dto_class_double).to receive(:new).and_return(dto_double)
+      allow(dto_double).to receive(:update_instance)
+      allow(parser_double).to receive(:parse).and_return([{}])
+    end
+
+    describe '#import' do
+      it 'parses the given data' do
+        importer.import(data: [{}])
+        expect(parser_double).to have_received(:parse)
+      end
+
+      it 'updates the database with the parsed data' do
+        importer.import(data: [])
+        expect(dto_class_double).to have_received(:new)
+        expect(dto_double).to have_received(:update_instance)
+      end
+    end
+  end
+
+end
