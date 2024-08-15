562bba492791dbb06aa003838e2540fdc5e0df10
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-08-15 13:27:31 +0100

    Add active status to decks unique per user

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -8,4 +8,31 @@ RSpec.describe Deck do
   it { is_expected.to have_many(:matches) }
   it { is_expected.to belong_to(:game) }
   it { is_expected.to belong_to(:archetype) }
+
+  describe '#active' do
+    describe 'when updating a users active deck' do
+      let(:user_id) { 'abcd-efgh' }
+      let(:decks) { create_list(:deck, 3, user_id:, active: false) }
+
+      it 'sets the deck as active' do
+        deck = decks.sample
+        deck.update(active: true)
+        expect(deck.reload).to be_active
+      end
+
+      describe 'when the user had a previous active deck' do
+        let!(:active_deck) do
+          deck = decks.first
+          deck.update(active: true)
+          deck
+        end
+
+        it 'sets the previous deck as inactive' do
+          deck = decks.second
+          deck.update(active: true)
+          expect(active_deck.reload).not_to be_active
+        end
+      end
+    end
+  end
 end
