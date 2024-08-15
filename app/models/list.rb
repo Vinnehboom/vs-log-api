91136fd5b4fac8e825d79c5b3df8845209dcbda7
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-08-15 13:34:15 +0100

    Add active status to lists scoped to deck

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,20 @@
 class List < ApplicationRecord
 
+  before_validation :ensure_single_deck_active, if: :active
+
+  scope :active, -> { where(active: true) }
+
   belongs_to :deck
+  has_many :matches, dependent: :nullify
+
   validates :name, presence: true
   validates :cards, presence: true
-  has_many :matches, dependent: :nullify
+  validates :active, uniqueness: { scope: :deck_id, conditions: -> { active } }
+
+  private
+
+  def ensure_single_deck_active
+    self.class.find_by(deck_id:, active: true)&.update(active: false)
+  end
 
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,4 +5,31 @@ RSpec.describe List do
   it { is_expected.to have_many(:matches) }
   it { is_expected.to validate_presence_of(:cards) }
   it { is_expected.to validate_presence_of(:name) }
+
+  describe '#active' do
+    describe 'when updating a decks active list' do
+      let(:deck) { create(:deck) }
+      let(:lists) { create_list(:list, 3, deck:, active: false) }
+
+      it 'sets the list as active' do
+        list = lists.sample
+        list.update(active: true)
+        expect(list.reload).to be_active
+      end
+
+      describe 'when the deck had a previous active list' do
+        let!(:active_list) do
+          list = lists.first
+          list.update(active: true)
+          list
+        end
+
+        it 'sets the previous list as inactive' do
+          list = lists.second
+          list.update(active: true)
+          expect(active_list.reload).not_to be_active
+        end
+      end
+    end
+  end
 end
