9ccd86a00f49bddae70eff6d44ab5c581886ee7e
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-08-18 11:38:29 +0100

    Add list create and update methods

--- app/controllers/concerns/decks/lists_controller.rb
+++ app/controllers/concerns/decks/lists_controller.rb
@@ -14,6 +14,24 @@ module Decks
       render json: @list
     end
 
+    def create
+      @list = lists.new(create_params)
+      if @list.save
+        render json: @list, status: :created
+      else
+        render json: @list.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @list = lists.find(params[:id])
+      if @list.update(update_params)
+        render json: @list
+      else
+        render json: @list.errors, status: :unprocessable_entity
+      end
+    end
+
     private
 
     def set_deck
@@ -26,6 +44,14 @@ module Decks
 
     def query_params
       params.permit(:active)
+    end
+
+    def create_params
+      params.require(:list).permit(:name, :active, cards: %i[name count])
+    end
+
+    def update_params
+      params.require(:list).permit(:name, :active)
     end
 
   end
