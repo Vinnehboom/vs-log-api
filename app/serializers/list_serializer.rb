e5fdefd4ee110d21e47aaf730d6e5399aea99e55
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-08-21 12:26:40 +0100

    Rename List cards relation

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -2,6 +2,6 @@ class ListSerializer < ActiveModel::Serializer
 
   attributes :id, :deck_id, :name, :active, :created_at, :updated_at
 
-  has_many :list_cards, if: -> { @instance_options[:include]&.include?(:list_cards) }
+  has_many :cards, if: -> { @instance_options[:include]&.include?(:cards) }
 
 end
--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     deck
     name { Faker::Cosmere.shard }
     after(:build) do |list, _context|
-      list.list_cards = build_list(:card, 1, list:, count: list.deck.game.deck_card_count)
+      list.cards = build_list(:card, 1, list:, count: list.deck.game.deck_card_count)
     end
 
     trait :skip_validate do
