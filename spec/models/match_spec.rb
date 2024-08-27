e66e54477d27c00c9a82407e9594d23bf932dbaf
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-08-27 10:44:42 +0100

    Add creation context for match_games validation
    In order to keep the import logic working

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -6,8 +6,30 @@ RSpec.describe Match do
   it { is_expected.to belong_to(:opponent_archetype) }
   it { is_expected.to have_many(:match_games) }
   it { is_expected.to belong_to(:list).optional }
-  it { is_expected.to validate_inclusion_of(:result).in_array(%w[W L T ID WW WL LW LL WLW WLL LWW LWL]) }
-  it { is_expected.to validate_presence_of(:match_games) }
+
+  describe '#match_games presence' do
+    describe 'when the context is during api creation' do
+      describe 'when the match has no games' do
+        subject do
+          match = build(:match)
+          match.match_games = []
+          match
+        end
+
+        it { is_expected.not_to be_valid(:api_create) }
+      end
+
+      describe 'when the match has games' do
+        subject do
+          match = build(:match)
+          match.match_games = build_list(:match_game, 1)
+          match
+        end
+
+        it { is_expected.to be_valid(:api_create) }
+      end
+    end
+  end
 
   describe 'match games count validation' do
     describe 'when the match is bo3' do
